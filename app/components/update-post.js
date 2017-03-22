fba307ad2de44adb58b974fa6e35d0bb593c4e33
Renee Mei and Tammy Dang <renee+tammy@>
2017-03-22 11:59:24 -0700

    add styling

---
+++ app/components/update-post.js
@@ -0,0 +1,26 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateFormShow: false,
+  actions: {
+    showUpdateForm() {
+      this.set('updateFormShow', true);
+    },
+    update(post) {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        date: this.get('date'),
+        image: this.get('image'),
+        content: this.get('content'),
+      };
+      this.set('updateFormShow', false);
+      this.sendAction('update', post, params);
+    },
+    delete(post) {
+      if (confirm('Are you sure you want to delete this rental?')){
+        this.sendAction('delete', post);
+      }
+    }
+  }
+});
--- app/routes/post.js
+++ app/routes/post.js
@@ -2,6 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('post', params.post_id); 
+    return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    update(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !==undefined) {
+          post.set(key,params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('index');
+    },
+    delete(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
