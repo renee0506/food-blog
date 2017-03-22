d23738d73122740446e8cf2dbf27ae48a131eefb
Renee Mei and Tammy Dang <renee+tammy@>
2017-03-22 10:10:29 -0700

    add initial files, create home, about, and contact page, add function to add a new blog post

---
+++ app/components/new-post.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  formShow: false,
+  actions: {
+    savePost() {
+      var params={
+        title: this.get('title'),
+        author: this.get('author'),
+        date: this.get('date'),
+        image: this.get('image'),
+        content: this.get('content'),
+      };
+      this.set('formShow', false);
+      this.sendAction('savePost', params);
+    },
+
+    showPostForm() {
+      this.set('formShow', true);
+    }
+  }
+});
---
+++ app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('post', {path: '/post/:post_id'});
+  this.route('about');
+  this.route('contact');
+});
+
+export default Router;
